95c78dde22c855816468100388e474c8c2a5a29b
ImGosuh <cosmoscrew1@gmail.com>
2019-09-02 14:59:32 -0500

    Add target _blank

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
                     <div className="mx-auto text-center">
                         <p className="text-footer">NoteMash de ScriptMash | &copy; Copyright 2019, Todos los derechos reservados</p>
                         <p>
-                            <a className="icon-contact" href="https://www.facebook.com/ScriptMash-112988803409290/" rel="noopener noreferrer">
+                            <a className="icon-contact" href="https://www.facebook.com/ScriptMash-112988803409290/" rel="noopener noreferrer" target="_blank">
                                 <i className="fab fa-facebook-square fa-2x"></i>
                             </a>
-                            <a className="icon-contact" href="https://www.linkedin.com/in/brandon-garc%C3%ADa-s%C3%A1nchez-991264186/" rel="noopener noreferrer">
+                            <a className="icon-contact" href="https://www.linkedin.com/in/brandon-garc%C3%ADa-s%C3%A1nchez-991264186/" rel="noopener noreferrer" target="_blank">
                                 <i className="fab fa-linkedin fa-2x"></i>
                             </a>
                         </p>
