5ea16e31ff97c03877e584870efaf203ce940f99
ImGosuh <cosmoscrew1@gmail.com>
2019-09-08 21:48:34 -0500

    Edit session

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 31536000 // 1 year
+        maxAge: 31536000 * 1000 // 1 year
     },
     store: new MongooseStore({
         connection: mongoose
