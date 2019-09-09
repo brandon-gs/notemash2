93526b504244c2e4af6ae9feb9d9fc1d8fd4ce6e
ImGosuh <cosmoscrew1@gmail.com>
2019-09-08 21:45:21 -0500

    Add 1 year session

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+        maxAge: 31536000 // 1 year
     },
     store: new MongooseStore({
         connection: mongoose
